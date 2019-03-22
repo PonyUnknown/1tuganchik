02b61703dbdba9287d53b170e72b324c8bf7f839
PonyUnknown <47217749+PonyUnknown@users.noreply.github.com>
2019-03-22 22:25:37 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import ProfileJs from "./pages/profile/Profile";
+
+export default class App extends Component {
+    state = { 
+        profiles: [
+            {nickname: 'Аширов Альдияр', name: 'Альдияр', surname: 'Аширов', nationality: 'Казах', sex: 'Мужской', age: '16', address: 'Проспект Манаса, 7',},
+        ]
+     }
+    render() {
+        let {profiles} = this.state;
+        return (
+            <div>
+                <ProfileJs profile={profiles[0]}/>
+            </div>
+        );
+    }
+}
+
+
+
+// webpack, reactBuild
+
+// const request = async () => {
+//     const response = await fetch('https://api.com/values/1');
+//     const json = await response.json();
+//     console.log(json);
+// }
+
+// request();
